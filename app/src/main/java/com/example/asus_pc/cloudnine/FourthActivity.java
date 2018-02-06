82bf4a2b67915cc0bdee7c911fd21848d8531b24
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2018-02-06 12:46:42 +0600

    Finding a webpage into this class

--- app/src/main/java/com/example/asus_pc/cloudnine/FourthActivity.java
+++ app/src/main/java/com/example/asus_pc/cloudnine/FourthActivity.java
@@ -2,12 +2,26 @@ package com.example.asus_pc.cloudnine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class FourthActivity extends AppCompatActivity {
+    private WebView webView;
+    ThirdActivity th = new ThirdActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+
+
+        webView = (WebView) findViewById(R.id.webId);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        String str = getIntent().getStringExtra("NAME");
+        webView.loadUrl("http://"+str+"/");
     }
 }
